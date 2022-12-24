2c0d4116e0edf9a43e3c0a5f1cf3490768653bac
Shivansh-soni <shivanshsoni.indore@gmail.com>
2022-12-24 20:45:08 +0530

    CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,7 @@ const app = express();
 //Enable body parser
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/openai", require("./routes/OpenaiRoutes"));
