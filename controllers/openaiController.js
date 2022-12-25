63002dff36a4bf8ea1a99f8084fd3f9527da0a26
Shivansh-soni <shivanshsoni.indore@gmail.com>
2022-12-25 14:38:33 +0530

    resolution fix

--- controllers/openaiController.js
+++ controllers/openaiController.js
@@ -9,13 +9,11 @@ const openai = new OpenAIApi(configuration);
 const generateImage = async (req, res) => {
   const { prompt, size } = req.body;
 
-  const imageSize =
-    size === "small" ? "256x256" : size === "large" ? "1024x1024" : "600x600";
   try {
     const response = await openai.createImage({
       prompt,
       n: 1,
-      size: imageSize,
+      size: "512x512",
     });
 
     const imageUrl = response.data.data[0].url;
